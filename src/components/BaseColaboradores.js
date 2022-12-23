c07e8956444415dfe5583cefad66d44c319f1453
luis <es.luiss89@gmail.com>
2022-12-22 22:01:17 -0300

    primero

---
+++ src/components/BaseColaboradores.js
@@ -0,0 +1,18 @@
+const BaseColaboradores = [
+    {
+        id: "1",
+        nombre: "Luis Espinoza",
+        correo: "lespinoza@gmail.com"
+    },
+    {
+        id: "2",
+        nombre: "Naruto Usumaki",
+        correo: "nusumaki@gmail.com"
+    },
+    {
+        id: "3",
+        nombre: "Kakashi Hatake",
+        correo: "khatake@gmail.com"
+    },
+]
+export default BaseColaboradores;
