d98030b1006d7ffedd12c8d8679ff6119bf2d8cb
luis <es.luiss89@gmail.com>
2022-12-22 22:12:30 -0300

    segundo

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { render } from 'react-dom'; // Importamos la función render de react-dom
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// No es necesario utilizar ReactDOM.createRoot, podemos usar la función render directamente
+render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
